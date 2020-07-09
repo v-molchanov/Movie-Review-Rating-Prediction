013d778d4bdfa3dd0e0880d1af01324ead18770a
Vladislav Molchanov <vlaaad20@rambler.ru>
2020-07-09 04:49:23 +0300

    Update urls.py

--- movie_reviews_site/movie_reviews_site/urls.py
+++ movie_reviews_site/movie_reviews_site/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from movie_reviews import views
+from movie_reviews_site.movie_reviews import views
 
 urlpatterns = [
     path('', views.index),
