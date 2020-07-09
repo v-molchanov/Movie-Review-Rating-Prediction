0844b7bbd9b329b7b8bf2aa0d2ecb32a255f71d5
Vladislav Molchanov <vlaaad20@rambler.ru>
2020-07-09 05:28:01 +0300

    Update settings.py

--- movie_reviews_site/settings.py
+++ movie_reviews_site/settings.py
@@ -66,6 +66,22 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
