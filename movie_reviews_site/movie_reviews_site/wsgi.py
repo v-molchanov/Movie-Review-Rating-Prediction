8c572b3b5a2baa009093aa7a24416407ce921224
Vladislav Molchanov <vlaaad20@rambler.ru>
2020-07-09 04:44:41 +0300

    Update wsgi.py

--- movie_reviews_site/movie_reviews_site/wsgi.py
+++ movie_reviews_site/movie_reviews_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_reviews_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_reviews_site.movie_reviews_site.settings')
 
 application = get_wsgi_application()
